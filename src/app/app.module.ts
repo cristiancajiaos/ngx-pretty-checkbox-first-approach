404553d13c654385447cc2c00378c28b7e9fca67
Cristian Cajiao <cristiancajiaos@outlook.com>
2023-01-25 13:23:16 -0300

    Instaladas dependencias de NGX Pretty Checkbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+import { NgxPrettyCheckboxModule } from 'ngx-pretty-checkbox';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { LayoutComponent } from './layout/layout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxPrettyCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
